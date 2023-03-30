ae95507d9e5c8fd5f7612b51576097c892525463
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-03-30 15:22:15 +0900

    friend Screen 세팅

---
+++ lib/components/profile_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao_ui/models/user.dart';
+
+class ProfileCard extends StatelessWidget {
+  final User user; //초기화 하기 위한 데이터. 바뀌지 않는 데이터임 !!!
+  const ProfileCard({Key? key, required this.user}) : super(key: key);
+  //같은 데이터가 또 들어오면 new를 하지 않고 재사용하며, 다른 데이터가 들어오면 새로운 new를 한다.
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      child: ListTile(
+        leading: _avatar(),
+        title: _title(),
+        subtitle: subtitle(),
+      ),
+    );
+  }
+
+  Text subtitle() {
+    return Text(
+      user.intro,
+      style: TextStyle(
+        fontSize: 12
+      ),
+    );
+  }
+
+  Text _title() {
+    return Text(
+      user.name,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16
+      ),
+    );
+  }
+
+  Widget _avatar() {
+    return CircleAvatar(
+      radius: 20,
+      backgroundImage: NetworkImage(
+        user.backgroundImage
+      ),
+    );
+  }
+}
--- lib/screens/friend_screen.dart
+++ lib/screens/friend_screen.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao_ui/components/profile_card.dart';
+import 'package:flutter_kakao_ui/models/user.dart';
 
 class FriendScreen extends StatelessWidget {
   const FriendScreen({Key? key}) : super(key: key);
@@ -8,7 +9,36 @@ class FriendScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("친구"),),
+      appBar: AppBar(title: Text("친구")),
+      body: Column( // 얘는 이제 body.dart로 새로 클래스를 그려서 빼서 리펙토링 하면 좋다.
+        children: [
+          const SizedBox(height: 10),
+          ProfileCard(user: me),
+          const Divider(),
+          Padding(
+            padding:
+                const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+            child: Row(
+              children: [
+                Text("친구"),
+                SizedBox(
+                  width: 10,
+                ),
+                Text("${friends.length}")
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: friends.length,
+              //개수를 알고 있고, 프로필 카드의 높이를 분석해서 화면에 맞게 로딩해준다.
+              //그리고 만약 스크롤 하면, 안보이는 프로필 카드에서 데이터만 갈아 끼워서 다시 보여준다.
+              itemBuilder: (context, index) =>
+                  ProfileCard(user: friends[index]),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
