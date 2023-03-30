ea94fdabf2c40038a0b6cdd3133e99b0a7c805e8
S-Sojung <80329856+S-Sojung@users.noreply.github.com>
2023-03-30 18:40:18 +0900

    채팅 완료

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,13 +1,22 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao_ui/components/chat_card.dart';
+import 'package:flutter_kakao_ui/models/chat.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("ChatS Screen"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("채팅"),
+      ),
+      body: ListView(
+        children:
+          List.generate(chats.length, (index) => ChatCard(chat: chats[index]))
+      ),
     );
   }
 }
